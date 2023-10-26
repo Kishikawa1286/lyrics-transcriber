acda3183b939eaf3cc910cc8c8ce3b1fb81ca473
Kishikawa1286 <sanbasan.dev@gmail.com>
2023-10-26 05:27:24 +0000

    fix firebase configuration

--- firebase/hosting-predeploy.sh
+++ firebase/hosting-predeploy.sh
@@ -3,9 +3,13 @@
 # This script is run by the firebase predeploy hook
 # See firebase.json
 
+SCRIPT_DIR=$(dirname $(readlink -f "$0"))
+
+FIREBASERC_PATH="${SCRIPT_DIR}/.firebaserc"
+
 project_to_alias() {
   PROJECT_NAME=$1
-  ALIAS_NAME=$(jq -r "to_entries[] | select(.value == \"$PROJECT_NAME\").key" $FIREBASERC_PATH)
+  ALIAS_NAME=$(jq -r ".projects | to_entries[] | select(.value == \"$PROJECT_NAME\").key" $FIREBASERC_PATH)
 
   if [ -z "$ALIAS_NAME" ]; then
     exit 1
@@ -15,7 +19,12 @@ project_to_alias() {
   fi
 }
 
-rm -rf \"$RESOURCE_DIR\"
+if [ -z "$GCLOUD_PROJECT" ] || [ -z "$RESOURCE_DIR" ]; then
+  echo "Error: Required environment variables are not set."
+  exit 1
+fi
+
+rm -rf $RESOURCE_DIR
 
 cd ../flutter
 ALIAS=$(project_to_alias $GCLOUD_PROJECT)
