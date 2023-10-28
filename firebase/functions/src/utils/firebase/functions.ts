b6b7d8d950025bc59b2d9a035a57713722e18884
Kishikawa1286 <sanbasan.dev@gmail.com>
2023-10-28 00:16:51 +0000

    [WIP] Firebase Functions

---
+++ firebase/functions/src/utils/firebase/functions.ts
@@ -0,0 +1,36 @@
+import {
+  MemoryOption,
+  database,
+  firestore,
+  https,
+  pubsub,
+  setGlobalOptions,
+  storage,
+} from "firebase-functions/v2";
+
+process.env.TZ = "Asia/Tokyo";
+
+type FunctionSecrets = "HUGGINGFACE_API_KEY";
+
+export const functions = ({
+  secrets,
+  memory,
+}: {
+  secrets: FunctionSecrets[];
+  memory: MemoryOption;
+}): {
+  database: typeof database;
+  https: typeof https;
+  pubsub: typeof pubsub;
+  firestore: typeof firestore;
+  storage: typeof storage;
+} => {
+  setGlobalOptions({ memory, region: "asia-northeast1", secrets });
+  return {
+    database,
+    https,
+    pubsub,
+    firestore,
+    storage,
+  };
+};
